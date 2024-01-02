9af927d15da059f1bb094534b4b576b8877218ee
Orbital <orbitalturtle@protonmail.com>
2024-01-01 18:40:40 -0600

    offers: send invoice request

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,11 +1,16 @@
 mod common;
 use lndk;
 
-use bitcoin::secp256k1::PublicKey;
+use bitcoin::secp256k1::{PublicKey, Secp256k1};
+use bitcoin::Network;
 use chrono::Utc;
 use ldk_sample::node_api::Node as LdkNode;
+use lightning::blinded_path::BlindedPath;
+use lightning::offers::offer::Quantity;
+use lndk::onion_messenger::MessengerUtilities;
 use std::path::PathBuf;
 use std::str::FromStr;
+use std::time::SystemTime;
 use tokio::select;
 use tokio::time::{sleep, timeout, Duration};
 
@@ -33,7 +38,6 @@ async fn wait_to_receive_onion_message(
 async fn check_for_message(ldk: LdkNode) -> LdkNode {
     loop {
         if ldk.onion_message_handler.messages.lock().unwrap().len() == 1 {
-            println!("MESSAGE: {:?}", ldk.onion_message_handler.messages);
             return ldk;
         }
         sleep(Duration::from_secs(2)).await;
@@ -87,6 +91,83 @@ async fn test_lndk_forwards_onion_message() {
         },
         // We wait for ldk2 to receive the onion message.
         (ldk1, ldk2) = wait_to_receive_onion_message(ldk1, ldk2, PublicKey::from_str(&lnd_info.identity_pubkey).unwrap()) => {
+            shutdown.trigger();
+            ldk1.stop().await;
+            ldk2.stop().await;
+        }
+    }
+}
+
+#[tokio::test(flavor = "multi_thread")]
+// Here we test the beginning of the BOLT 12 offers flow. We show that lndk successfully builds an
+// invoice_request and sends it.
+async fn test_lndk_send_invoice_request() {
+    let test_name = "lndk_send_invoice_request";
+    let (_bitcoind, mut lnd, ldk1, ldk2, lndk_dir) =
+        common::setup_test_infrastructure(test_name).await;
+
+    // Here we'll produce a little network path:
+    //
+    // ldk1 <-> ldk2 <-> lnd
+    //
+    // ldk1 will be the offer creator, which will build a blinded route from ldk2 to ldk1.
+    let (pubkey, _) = ldk1.get_node_info();
+    let (pubkey_2, addr_2) = ldk2.get_node_info();
+    let lnd_info = lnd.get_info().await;
+    let lnd_pubkey = PublicKey::from_str(&lnd_info.identity_pubkey).unwrap();
+
+    ldk1.connect_to_peer(pubkey_2, addr_2).await.unwrap();
+    lnd.connect_to_peer(pubkey_2, addr_2).await;
+
+    let path_pubkeys = vec![pubkey_2, pubkey];
+    let expiration = SystemTime::now() + Duration::from_secs(24 * 60 * 60);
+    let offer = ldk1
+        .create_offer(
+            &path_pubkeys,
+            Network::Regtest,
+            20_000,
+            Quantity::One,
+            expiration,
+        )
+        .await
+        .expect("should create offer");
+
+    // Now we'll spin up lndk, which should forward the invoice request to ldk2.
+    let (shutdown, listener) = triggered::trigger();
+    let lnd_cfg = lndk::lnd::LndCfg::new(
+        lnd.address.clone(),
+        PathBuf::from_str(&lnd.cert_path).unwrap(),
+        PathBuf::from_str(&lnd.macaroon_path).unwrap(),
+    );
+    let lndk_cfg = lndk::Cfg {
+        lnd: lnd_cfg,
+        log_dir: Some(
+            lndk_dir
+                .join(format!("lndk-logs.txt"))
+                .to_str()
+                .unwrap()
+                .to_string(),
+        ),
+        shutdown: shutdown.clone(),
+        listener,
+    };
+
+    let client = lnd.client.clone().unwrap();
+    let blinded_path = offer.paths()[0].clone();
+    let messenger_utils = MessengerUtilities::new();
+    let secp_ctx = Secp256k1::new();
+    let reply_path =
+        BlindedPath::new_for_message(&[pubkey_2, lnd_pubkey], &messenger_utils, &secp_ctx).unwrap();
+
+    // Make sure lndk successfully sends the invoice_request.
+    let handler = &mut lndk::OfferHandler::new();
+    select! {
+        val = handler.run(lndk_cfg.clone()) => {
+            panic!("lndk should not have completed first {:?}", val);
+        },
+        // We wait for ldk2 to receive the onion message.
+        res = handler.pay_offer(offer.clone(), Some(20_000), Network::Regtest, client.clone(), blinded_path.clone(), Some(reply_path)) => {
+            assert!(res.is_ok());
             shutdown.trigger();
             ldk1.stop().await;
             ldk2.stop().await;
